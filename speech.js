d59c20e1e59986dd69bda9ac55e65c25fc942549
Mark Dooney <desmarkie@gmail.com>
2018-01-11 14:52:42 +0000

    Update speech.js

--- speech.js
+++ speech.js
@@ -4,8 +4,10 @@
   
   Simple wrapper for the WebSpeech API ( https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API )
 
-  I found the standard API had issues with the continuous setting crashing the api when returning the second result, so created this helper to restart after every result in order to mimic the behaviour of an 'always on' mode.
-  After a few minutes of running it can generates a 'network' error that also causes the api to stop responding. The only fix for that I could find was to recreate the SpeechRecogniton completely, so I dropped in the restartCallback to allow handling of this.
+  I found the standard API had issues with the continuous setting crashing the api when returning the second result, 
+  so created this helper to restart after every result in order to mimic the behaviour of an 'always on' mode.
+  After a few minutes of running it can generates a 'network' error that also causes the api to stop responding. 
+  The only fix for that I could find was to recreate the SpeechRecogniton object.
 
   Usage:
 
